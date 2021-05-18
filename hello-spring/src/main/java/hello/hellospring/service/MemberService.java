fce218f73e5373ea1a4db9627ac54b1f0cb10e80
iw0406 <hyunjilim98@gmail.com>
2021-05-18 22:36:26 +0900

    chap7

--- hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -22,7 +22,7 @@ public class MemberService {
     /**
      * 회원가입
      */
-    public Long join(Member member){
+    public Long join(Member member) {
         validateDuplicateMember(member); //중복 회원 검증
         memberRepository.save(member);
         return member.getId();
@@ -38,11 +38,11 @@ public class MemberService {
     /**
      * 전체 회원 조회
      */
-    public List<Member> findMembers(){
+    public List<Member> findMembers() {
         return memberRepository.findAll();
     }
 
-    public Optional<Member> findOne(Long memberId){
+    public Optional<Member> findOne(Long memberId) {
         return memberRepository.findById(memberId);
     }
 }
