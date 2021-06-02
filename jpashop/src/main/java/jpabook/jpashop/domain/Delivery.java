d81008bcfcc68646bdfe4f2be5bc0517aac60fb0
iw0406 <hyunjilim98@gmail.com>
2021-06-02 14:32:17 +0900

    chap3 - 도메인 설계

---
+++ jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) //ORDINAL은 숫자로 저장해서 밀릴 위험이 있음
+    private DeliveryStatus deliveryStatus; //READY, COMP
+}
