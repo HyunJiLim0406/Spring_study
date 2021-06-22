485a2dbebe8c062b2775541a6bec53ea9f5db795
iw0406 <hyunjilim98@gmail.com>
2021-06-22 16:18:59 +0900

    chap4 - OneToMany

--- jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -87,4 +87,14 @@ public class OrderRepository {
                         " join o.delivery d", OrderSimpleQueryDto.class
         ).getResultList();
     }
+
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o" + //페이징 불가능
+                        " join fetch o.member m"+
+                        " join fetch  o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                .getResultList();
+    }
 }
